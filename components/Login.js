590ba9b193634ed09ffa810cfda28128cc1b7137
Likhith68 <Likhithjsc@gmail.com>
2022-06-30 11:04:13 +0530

    next auth

--- components/Login.js
+++ components/Login.js
@@ -3,21 +3,21 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
+    <div className="bg-gray-200">
       <div className="flex flex-col items-center min-h-screen justify-center -mt-2 `text-center">
-        <img
-          className="w-96"
-          src="https://t3.ftcdn.net/jpg/04/32/86/42/360_F_432864205_aaLreq5kBDWUqDqgczdEM0aVP1bXCTTc.jpg"
-          alt=""
-        />
-        <p className="ml-8 mt-5 font-xs italic">
-          Facebook Clone--Built For Educational Purposes Only{" "}
-        </p>
-        <div className="mt-5">
-          <div className="mt-3">
+        <div className="bg-white p-8 rounded-xl">
+          <img
+            className="w-96"
+            src="https://t3.ftcdn.net/jpg/04/32/86/42/360_F_432864205_aaLreq5kBDWUqDqgczdEM0aVP1bXCTTc.jpg"
+            alt=""
+          />
+          <p className="ml-8 mt-5 font-xs italic">
+            Facebook Clone--Built For Educational Purposes Only{" "}
+          </p>
+          <div className="ml-40">
             <button
               onClick={signIn}
-              className="p-2 pl-3 pr-3 rounded-md normal-case bg-blue-500 text-white font-sans hover:bg-blue-800"
+              className="p-2 pl-3 pr-3 rounded-full mt-8 normal-case bg-blue-500 text-white font-sans hover:bg-blue-800"
             >
               Sign in
             </button>
