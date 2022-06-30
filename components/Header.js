c83fa476a35b4382c6a81da906e74d511a1a8889
Likhith68 <Likhithjsc@gmail.com>
2022-06-30 09:54:48 +0530

    trying Link Again

--- components/Header.js
+++ components/Header.js
@@ -58,7 +58,7 @@ function Header() {
       {session ? (
         <>
           <div className="flex items-center justify-end space-x-2 md:space-x-3 lg:space-x-4 ">
-            <a
+            <Link
               className="hover:bg-gray-200 rounded-full flex p-2 items-center justify-end space-x-2 md:space-x-3 lg:space-x-4"
               href="/auth/signin"
             >
@@ -72,7 +72,7 @@ function Header() {
                   {session?.user?.name}
                 </p>
               </div>
-            </a>
+            </Link>
 
             <ViewGridIcon className="icon" />
             <ChatIcon className="icon" />
